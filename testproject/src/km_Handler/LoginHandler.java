b0e6366f0c1d0aaca1ea89d45d0c714a23d832a0
phile <phile@192.168.1.5>
2017-01-17 17:20:12 +0100

    fixed wrong dependencies due to wrong apache settings plus minor changes on login

--- testproject/src/km_Handler/LoginHandler.java
+++ testproject/src/km_Handler/LoginHandler.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 
-@ManagedBean(name="loginHandler")
+@ManagedBean(name="loginHandler", eager=true)
 @SessionScoped
 public class LoginHandler implements Serializable{
 	/**
@@ -24,6 +24,7 @@ public class LoginHandler implements Serializable{
 	private String email;
 	private String password;
 	private UserView user;	//deleted get/set for security reasons
+	
 	private boolean loggedIn;
 
 	@ManagedProperty("#{serviceLocatorImpl}")
@@ -32,26 +33,37 @@ public class LoginHandler implements Serializable{
 	@ManagedProperty("#{loginServiceImpl}")
 	private LoginService loginservice;
 	
+	public LoginService getLoginservice() {
+		return loginservice;
+	}
+	public void setLoginservice(LoginService loginservice) {
+		this.loginservice = loginservice;
+	}
 	public ServiceLocator getServiceLocator() { return serviceLocator; }
 	public void setServiceLocator(ServiceLocator serviceLocatorBean) { this.serviceLocator = serviceLocatorBean; }
 		
 	public String login() {
+		System.out.println("in login");
 		System.out.println("Login attempt by"+email+ " " + password); //TO DELETE
 		user = loginservice.getLogin(email, password);
 		FacesContext context = FacesContext.getCurrentInstance();
 		if(user != null){
 			context.getExternalContext().getSessionMap().put("user", user);
+			loggedIn = true;
 			return "loggedin";
 		}
 		context.addMessage(null, new FacesMessage("Unknown login, try again"));
         email = null;
         password = null;
-		return null;
+        loggedIn = false;
+		return "fail";
 	}	
 	
 	public void validateEmail(FacesContext context, UIComponent component, Object value)
 			throws ValidatorException {
 		if(!((String) value).matches(".+@.\\..+")){
+
+			System.out.println("in validateemail");
 			throw new ValidatorException(
 					new FacesMessage("Fehlerhafte Emailsyntax!"));
 		}
@@ -59,6 +71,8 @@ public class LoginHandler implements Serializable{
 
 	public void validatePassword(FacesContext context, UIComponent component, Object value)
 			throws ValidatorException {
+
+		System.out.println("in validatepassword");
 		if(!(((String) value).length()<6)){
 			throw new ValidatorException(
 					new FacesMessage("Passwort zu kurz!"));
